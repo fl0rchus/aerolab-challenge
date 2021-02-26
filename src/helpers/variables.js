e303889e0a16a290dcc33ed17caad468e0f1de7f
Florencia Quinteros <56704793+fl0rchus@users.noreply.github.com>
2021-02-26 20:00:27 -0300

    token v2

--- src/helpers/variables.js
+++ src/helpers/variables.js
@@ -1,7 +1,7 @@
 export const headers = {
   "Content-Type": "application/json",
   Authorization:
-    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDBmNTI2MTdlNzE4NzAwMjBlMzhlZTQiLCJpYXQiOjE2MTE2MTY4NjV9.66smQBQU8yIdqHDJyl9efNAbdagrgh9iU7Jf-KSKfXw",
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDM5NjdmYjdlNzE4NzAwMjBlMzhmMzYiLCJpYXQiOjE2MTQzNzQ5MDd9.SLgYFeL3Rjdky2fXnV-lxUEZA9nLY6O6otK1MB2znRo",
 };
 
 export const urlUser = "https://coding-challenge-api.aerolab.co/user/me";
